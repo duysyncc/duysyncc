6c0f39eb7b9742ae086c2ce41f0bd3f336c84c6a
unknown <duyduysysy@gmail.com>
2021-12-28 11:18:04 +0700

    123

--- src/App.js
+++ src/App.js
@@ -10,40 +10,67 @@ function App() {
   // const [url, setUrl] = useState("/Swap")
   const [url, setUrl] = useState("/Swap");
   return (
-    // <div className="App">
-    //   <ul className="router">
-    //     <li className="Logo">Hello Swap</li>
-    //     <li onClick={() => {
-    //       setUrl("/Swap")
-    //     }}>Swap</li>
+    <div className="App">
+      <ul className="router">
+        <li className="Logo">Hello Swap</li>
+        <li
+          onClick={() => {
+            setUrl("/Swap");
+          }}
+        >
+          Swap
+        </li>
 
-    //     <li onClick={() => {
-    //       setUrl("/Liquidity")
-    //     }}>Liquidity</li>
+        <li
+          onClick={() => {
+            setUrl("/Liquidity");
+          }}
+        >
+          Liquidity
+        </li>
 
-    //     <li onClick={() => {
-    //       setUrl("/Stake")
-    //     }}>Stake</li>
+        <li
+          onClick={() => {
+            setUrl("/Stake");
+          }}
+        >
+          Stake
+        </li>
 
-    //     <li onClick={() => {
-    //       setUrl("/Tx8Swap")
-    //     }}>Tx8Swap</li>
+        <li
+          onClick={() => {
+            setUrl("/Tx8Swap");
+          }}
+        >
+          Tx8Swap
+        </li>
 
-    //     <li onClick={() => {
-    //       setUrl("/Faucet")
-    //     }}>Faucet</li>
+        <li
+          onClick={() => {
+            setUrl("/Faucet");
+          }}
+        >
+          Faucet
+        </li>
 
-    //   </ul>
-    //   <div className="main">
-    //     {url == "/Swap" ? <SwapPage /> : null}
-    //     {url == "/Liquidity" ? <LiquidityPage /> : null}
-    //     {url == "/Faucet" ? <FaucetPage /> : null}
-    //     {url == "/Stake" ? <StakePage /> : null}
-    //     {url == "/Tx8Swap" ? <Tx8SwapPage /> : null}
-    //     {url == "/CrawlPage" ? <CrawlPage /> : null}
-    //   </div>
-    // </div>
-    <CrawlPage />
+        <li
+          onClick={() => {
+            setUrl("/CrawlPage");
+          }}
+        >
+          CrawlPage
+        </li>
+      </ul>
+      <div className="main">
+        {url == "/Swap" ? <SwapPage /> : null}
+        {url == "/Liquidity" ? <LiquidityPage /> : null}
+        {url == "/Faucet" ? <FaucetPage /> : null}
+        {url == "/Stake" ? <StakePage /> : null}
+        {url == "/Tx8Swap" ? <Tx8SwapPage /> : null}
+        {url == "/CrawlPage" ? <CrawlPage /> : null}
+      </div>
+    </div>
+    // <CrawlPage />
   );
 }
 
